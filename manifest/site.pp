fb24e24a030e0093e0a33b81bb420f65fb36650b
Adequnl3 <50166329+Adequnl3@users.noreply.github.com>
2020-04-02 18:44:30 +0100

    Update site.pp

--- manifest/site.pp
+++ manifest/site.pp
@@ -1,4 +1,9 @@
 node default {
+  file { '/root/README':
+    ensure => file,
+    content => 'This is a readme',
+    owner   => 'root',
+   }
   }
   node 'automachine.eastus.cloudapp.azure.com' {
     include role::master_server
