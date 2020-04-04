afce91e1fc6f26bf8a0816394f9275382e4fc5af
Adequnl3 <50166329+Adequnl3@users.noreply.github.com>
2020-04-04 12:31:11 +0100

    Update agents_nodes.pp

--- site/profile/manifests/agents_nodes.pp
+++ site/profile/manifests/agents_nodes.pp
@@ -2,4 +2,7 @@ class profile::agent_nodes {
   include dockeragent
   dockeragent:: {'web.puppet.vm'}
   dockeragent:: {'db.puppet.vm'}
+  host {'web.puppet.vm':
+    ensure => present,
+    ip     => '10.1.0.5',
   }
